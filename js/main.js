b183ed1e924e0556bef92e6c779d6c63722a6cee
chakapega <chakapega@gmail.com>
2020-09-02 02:52:51 +0300

    feat: added mobile menu buttons

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ const menuItemElementTwo = document.querySelector(".menu__item-element-two");
 const menuItemElementFour = document.querySelector(".menu__item-element-four");
 const subMenuElementTwo = document.querySelector(".sub-menu-element-two");
 const subMenuElementFour = document.querySelector(".sub-menu-element-four");
+const mobileMenuButton = document.querySelector(".mobile-menu-button");
+const mobileMenuButtonIcon = document.querySelector(".mobile-menu-button-icon");
+const mobileMenuButtonIconClose = document.querySelector(
+  ".mobile-menu-button-icon-close"
+);
 
 menuItemElementTwo.addEventListener("mouseenter", () => {
   subMenuElementTwo.classList.remove("sub-menu-element-two_hidden");
@@ -17,4 +22,11 @@ menuItemElementFour.addEventListener("mouseenter", () => {
 
 menuItemElementFour.addEventListener("mouseleave", () => {
   subMenuElementFour.classList.add("sub-menu-element-four_hidden");
+});
+
+mobileMenuButton.addEventListener("click", () => {
+  mobileMenuButtonIcon.classList.toggle("mobile-menu-button-icon_hidden");
+  mobileMenuButtonIconClose.classList.toggle(
+    "mobile-menu-button-icon-close_hidden"
+  );
 });
